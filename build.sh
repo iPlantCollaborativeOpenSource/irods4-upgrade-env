70f4b303d7b951db82ddbc1a8838a77c3a30aa63
Tony Edgin <tedgin@iplantcollaborative.org>
2015-03-04 08:22:04 -0700

    build script removes dangling images

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-docker build --file icat-db.Dockerfile --tag icat-db .
-docker build --file irods4.0.3-icat.Dockerfile --tag irods4.0.3-icat .
+docker build --file icat-db.Dockerfile               --tag icat-db .
+docker build --file irods4.0.3-icat.Dockerfile       --tag irods4.0.3-icat .
 docker build --file irods4.0.3-rs-centos5.Dockerfile --tag irods4.0.3-rs-centos5 .
 docker build --file irods4.0.3-rs-centos6.Dockerfile --tag irods4.0.3-rs-centos6 .
-docker build --file irods4.0.3-rs-ubuntu.Dockerfile --tag irods4.0.3-rs-ubuntu .
-docker build --file icommands4.0.3.Dockerfile --tag icommands4.0.3 .
+docker build --file irods4.0.3-rs-ubuntu.Dockerfile  --tag irods4.0.3-rs-ubuntu .
+docker build --file icommands4.0.3.Dockerfile        --tag icommands4.0.3 .
+
+docker rmi $(docker images --quiet --filter 'dangling=true')
