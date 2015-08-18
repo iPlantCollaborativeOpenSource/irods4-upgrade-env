0f8b1d11b25c69678b59e54779c73b3c225bcd1b
Tony Edgin <tedgin@iplantcollaborative.org>
2015-08-18 09:03:52 -0700

    CORE-6961 moved user creation to db init dirs

---
+++ irods3.3.1-dbms/create-users.sql
@@ -0,0 +1,2 @@
+CREATE USER icat_reader WITH PASSWORD 'password';
+CREATE USER icat_reader_mirrors WITH CONNECTION LIMIT 250 PASSWORD 'password';
--- up.sh
+++ up.sh
@@ -2,32 +2,6 @@
 
 # This scripts brings up the iRODS grid. 
 
-readonly DBMS_CONTAINER=irods4upgradeenv_dbms_1
-
-
-dbms_exec ()
-{
-  eval "docker exec --user postgres $DBMS_CONTAINER $@"
-}
-
-
-db_cmd ()
-{
-  dbms_exec psql --command=\"$@\"
-}
-
-
-wait_for_dbms ()
-{
-  while ! dbms_exec psql --list 2> /dev/null >&2
-  do
-    sleep 1
-  done
-}
-
 
 docker-compose up -d --no-recreate ies
-wait_for_dbms
-db_cmd CREATE USER icat_reader WITH PASSWORD \'password\'
-db_cmd CREATE USER icat_reader_mirrors WITH CONNECTION LIMIT 250 PASSWORD \'password\'
 
