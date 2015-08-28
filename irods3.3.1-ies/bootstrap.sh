c0a0a46ba12192abd1bc768a731dcc9f2fd16b19
Tony Edgin <tedgin@iplantcollaborative.org>
2015-08-28 14:52:27 -0700

    CORE-6962 refactoring
    Fixed aegisVault mounts permission error
    Moved index creation to IES bootstrap script

--- irods3.3.1-ies/bootstrap.sh
+++ irods3.3.1-ies/bootstrap.sh
@@ -2,92 +2,95 @@
 
 if [ -z "$DBMS_NAME" ]
 then
-    echo "An irods-dbms container needs to be linked to 'dbms'" 1>&2
-    exit 1
+  echo "An irods-dbms container needs to be linked to 'dbms'" 1>&2
+  exit 1
 fi
 
 if [ -z "$POSTGRES_PASSWORD" ]
 then
-    echo "The environment variable POSTGRES_PASSWORD wasn't set" 1>&2
-    exit 1
+  echo "The environment variable POSTGRES_PASSWORD wasn't set" 1>&2
+  exit 1
 fi
 
 if [ -z "$POSTGRES_USER" ]
 then
-    POSTGRES_USER=irods
+  POSTGRES_USER=irods
 fi
 
 if [ -z "$ADMIN_USER" ]
 then
-    ADMIN_USER=rods
+  ADMIN_USER=rods
 fi
 
 if [ -z "$ADMIN_PASSWORD" ]
 then
-    ADMIN_PASSWORD=rods
+  ADMIN_PASSWORD=rods
 fi
 
 if [ -z "$ZONE" ]
 then
-    ZONE=tempZone
+  ZONE=tempZone
 fi
 
 
 function mk_irods_config ()
 {
-    echo "# Database configuration"
-    echo ""
-    echo "\$DATABASE_TYPE = 'postgres';"
-    echo "\$DATABASE_ODBC_TYPE = 'unix';"
-    echo "\$DATABASE_EXCLUSIVE_TO_IRODS = '0';"
-    echo "\$DATABASE_HOME = '/usr/pgsql-9.0';"
-    echo "\$DATABASE_LIB = '';"
-    echo ""
-    echo "\$DATABASE_HOST = 'dbms';"
-    echo "\$DATABASE_PORT = '5432';"
-    echo "\$DATABASE_ADMIN_PASSWORD = '$POSTGRES_PASSWORD';"
-    echo "\$DATABASE_ADMIN_NAME = '$POSTGRES_USER';"
-    echo ""
-    echo "# iRODS configuration"
-    echo ""
-    echo "\$IRODS_HOME = '/home/irods/iRODS';"
-    echo "\$IRODS_PORT = '1247';"
-    echo "\$SVR_PORT_RANGE_START = '20000';"
-    echo "\$SVR_PORT_RANGE_END = '20399';"
-    echo "\$IRODS_ADMIN_NAME = '$ADMIN_USER';"
-    echo "\$IRODS_ADMIN_PASSWORD = '$ADMIN_PASSWORD';"
-    echo "\$IRODS_ICAT_HOST = '';"
-    echo ""
-    echo "\$DB_NAME = 'ICAT';"
-    echo "\$RESOURCE_NAME = 'demoResc';"
-    echo "\$RESOURCE_DIR = '/home/irods/iRODS/Vault';"
-    echo "\$ZONE_NAME = '$ZONE';"
-    echo "\$DB_KEY = '123';"
-    echo ""
-    echo "\$GSI_AUTH = '0';"
-    echo "\$GLOBUS_LOCATION = '';"
-    echo "\$GSI_INSTALL_TYPE = '';"
-    echo ""
-    echo "\$KRB_AUTH = '0';"
-    echo "\$KRB_LOCATION = '';"
-    echo ""
-    echo "# NCCS Audit Extensions"
-    echo ""
-    echo "\$AUDIT_EXT = '0';"
-    echo ""
-    echo "# UNICODE"
-    echo ""
-    echo "\$UNICODE = '0';"
-    echo ""
-    echo "return 1;"
+  echo "# Database configuration"
+  echo ""
+  echo "\$DATABASE_TYPE = 'postgres';"
+  echo "\$DATABASE_ODBC_TYPE = 'unix';"
+  echo "\$DATABASE_EXCLUSIVE_TO_IRODS = '0';"
+  echo "\$DATABASE_HOME = '/usr/pgsql-9.0';"
+  echo "\$DATABASE_LIB = '';"
+  echo ""
+  echo "\$DATABASE_HOST = 'dbms';"
+  echo "\$DATABASE_PORT = '5432';"
+  echo "\$DATABASE_ADMIN_PASSWORD = '$POSTGRES_PASSWORD';"
+  echo "\$DATABASE_ADMIN_NAME = '$POSTGRES_USER';"
+  echo ""
+  echo "# iRODS configuration"
+  echo ""
+  echo "\$IRODS_HOME = '/home/irods/iRODS';"
+  echo "\$IRODS_PORT = '1247';"
+  echo "\$SVR_PORT_RANGE_START = '20000';"
+  echo "\$SVR_PORT_RANGE_END = '20399';"
+  echo "\$IRODS_ADMIN_NAME = '$ADMIN_USER';"
+  echo "\$IRODS_ADMIN_PASSWORD = '$ADMIN_PASSWORD';"
+  echo "\$IRODS_ICAT_HOST = '';"
+  echo ""
+  echo "\$DB_NAME = 'ICAT';"
+  echo "\$RESOURCE_NAME = 'demoResc';"
+  echo "\$RESOURCE_DIR = '/home/irods/iRODS/Vault';"
+  echo "\$ZONE_NAME = '$ZONE';"
+  echo "\$DB_KEY = '123';"
+  echo ""
+  echo "\$GSI_AUTH = '0';"
+  echo "\$GLOBUS_LOCATION = '';"
+  echo "\$GSI_INSTALL_TYPE = '';"
+  echo ""
+  echo "\$KRB_AUTH = '0';"
+  echo "\$KRB_LOCATION = '';"
+  echo ""
+  echo "# NCCS Audit Extensions"
+  echo ""
+  echo "\$AUDIT_EXT = '0';"
+  echo ""
+  echo "# UNICODE"
+  echo ""
+  echo "\$UNICODE = '0';"
+  echo ""
+  echo "return 1;"
 }
 
 
 function setup_irods ()
 {
-    sudo -H -u irods sh -c "cd /home/irods/iRODS; yes | ./irodssetup"
+  sudo -H -u irods bash -c "cd /home/irods/iRODS; yes | ./irodssetup"
 }
 
+
+mkdir /home/irods/aegisVault/UA1 /home/irods/aegisVault/ASU1
+chown -R irods:irods /home/irods/aegisVault
 
 sed --in-place \
     "{
@@ -112,14 +115,14 @@ chown irods:irods /home/irods/iRODS/config/irods.config
 
 while true
 do
-    PGPASSWORD=$POSTGRES_PASSWORD psql --list --quiet --host dbms postgres $POSTGRES_USER
+  PGPASSWORD=$POSTGRES_PASSWORD psql --list --quiet --host dbms postgres $POSTGRES_USER
 
-    if [ $? -eq 0 ]
-    then
-        break
-    fi
+  if [ $? -eq 0 ]
+  then
+    break
+  fi
 
-    sleep 1
+  sleep 1
 done
 
 setup_irods
@@ -127,15 +130,26 @@ setup_irods
 # The iRODS server may not have started, try again
 if [ $? -ne 0 ]
 then
-    setup_irods
+  setup_irods
 fi
 
-sudo -H -u irods sh -c \
-    "export PATH=$PATH:/home/irods/iRODS/clients/icommands/bin
-     mkdir /home/irods/aegisVault/UA1 /home/irods/aegisVault/ASU1
+# Configure iPlant resources
+sudo -E -H -u irods bash -c \
+    "echo 'export PATH=\$PATH:/home/irods/iRODS/clients/icommands/bin' >> /home/irods/.bashrc
      iadmin atrg iplantRG demoResc
      iadmin mkresc aegisUA1Res 'unix file system' archive ies /home/irods/aegisVault/UA1
      iadmin mkresc aegisASU1Res 'unix file system' archive ies /home/irods/aegisVault/ASU1
      iadmin atrg aegisRG aegisASU1Res"
+
+# configure custom indices
+PGPASSWORD=$POSTGRES_PASSWORD psql --host dbms ICAT $POSTGRES_USER <<-EOSQL
+  CREATE INDEX idx_coll_main_coll_type ON r_coll_main (coll_type);
+  CREATE INDEX idx_coll_main_parent_coll_name ON r_coll_main (parent_coll_name);
+  CREATE INDEX idx_objt_access_access_type_id ON r_objt_access (access_type_id);
+  CREATE INDEX idx_objt_access_user_id ON r_objt_access (user_id);
+  CREATE INDEX idx_tokn_main_token_namespace ON r_tokn_main (token_namespace);
+  CREATE INDEX idx_user_main_user_type_name ON r_user_main (user_type_name);
+  CREATE INDEX idx_user_password_user_id ON r_user_password (user_id);
+EOSQL
 
 bash
--- up.sh
+++ up.sh
@@ -21,47 +21,19 @@ query_icat ()
 }
 
 
-wait_for_postgres () 
+wait_for_icat () 
 {
-  echo waiting for postgres
+  echo waiting for ICAT database
 
   until $(docker exec $DBMS_CONTAINER netstat -4 --listening --numeric --tcp | grep --silent 5432) 
   do
     sleep 1
   done
-}
-
-
-wait_for_icat ()
-{
-  echo waiting for ICAT database
 
   until $(dbms_psql --list | grep --silent ICAT)
   do
     sleep 1
   done
-}
-
-
-create_indices()
-{
-  for table in r_coll_main r_objt_access r_tokn_main r_user_main r_user_password
-  do
-    echo waiting for $table table
-
-    until $(query_icat '\dt' | grep --silent $table)
-    do
-      sleep 1
-    done
-  done
-
-  query_icat 'CREATE INDEX idx_coll_main_coll_type ON r_coll_main (coll_type)'
-  query_icat 'CREATE INDEX idx_coll_main_parent_coll_name ON r_coll_main (parent_coll_name)'
-  query_icat 'CREATE INDEX idx_objt_access_access_type_id ON r_objt_access (access_type_id)'
-  query_icat 'CREATE INDEX idx_objt_access_user_id ON r_objt_access (user_id)'
-  query_icat 'CREATE INDEX idx_tokn_main_token_namespace ON r_tokn_main (token_namespace)'
-  query_icat 'CREATE INDEX idx_user_main_user_type_name ON r_user_main (user_type_name)'
-  query_icat 'CREATE INDEX idx_user_password_user_id ON r_user_password (user_id)'
 }
 
 
@@ -78,11 +50,9 @@ add_icat_reader()
 
 prepare_dbms()
 {
-  wait_for_postgres
   wait_for_icat
   add_icat_reader icat_reader password 100
   add_icat_reader icat_reader_mirrors password 250
-  create_indices
 }
 
 
