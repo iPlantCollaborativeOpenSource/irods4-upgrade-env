e717894008651faafeace45111917bb8b410f6f3
Tony Edgin <tedgin@iplantcollaborative.org>
2016-06-30 14:29:35 -0700

    converted formatting of up.sh

--- up.sh
+++ up.sh
@@ -8,9 +8,9 @@ source env.properties
 
 container_for()
 {
-  service=$1
+  local service="$1"
 
-  echo ${PROJECT_NAME}_${service}_1
+  printf '%s_%s_1' "$PROJECT_NAME" "$service"
 }
 
 
@@ -22,20 +22,22 @@ dbms_psql()
 
 query_icat() 
 {
-  dbms_psql "--command=$1" ICAT
+  local cmd="$1"
+
+  dbms_psql --command="$cmd" ICAT
 }
 
 
 wait_for_service() 
 {
-  service=$1
-  port=$2
+  local service="$1"
+  local port="$2"
 
-  container=$(container_for $service)
+  local container=$(container_for "$service")
 
-  echo waiting for service on $service
+  printf 'waiting for service on %s\n' "$service"
 
-  until $(docker exec --interactive $container bash -c "exec <>/dev/tcp/localhost/$port" \
+  until $(docker exec --interactive "$container" bash -c "exec <>/dev/tcp/localhost/'$port'" \
           2>/dev/null) 
   do
     sleep 1
@@ -45,9 +47,9 @@ wait_for_service()
 
 add_icat_reader()
 {
-  user=$1
-  password=$2
-  connLimit=$3
+  local user="$1"
+  local password="$2"
+  local connLimit="$3"
 
   query_icat "CREATE USER $user WITH CONNECTION LIMIT $connLimit PASSWORD '$password'"
   query_icat "GRANT SELECT ON ALL TABLES IN SCHEMA public TO $user" 
@@ -58,7 +60,7 @@ prepare_dbms()
 {
   wait_for_service dbms 5432
 
-  echo waiting for ICAT database
+  printf 'waiting for ICAT database\n'
 
   until $(dbms_psql --list | grep --silent ICAT)
   do
@@ -72,34 +74,34 @@ prepare_dbms()
 
 start_resources()
 {
-  services="$@"
+  local services="$@"
 
   for service in $services
   do
-    docker exec $(container_for $service) touch /IES_UP
+    docker exec $(container_for "$service") touch /IES_UP
   done
 
   for service in $services
   do
-    wait_for_service $service 1247
+    wait_for_service "$service" 1247
   done
 }
  
 
-docker-compose --project-name $PROJECT_NAME up -d --no-recreate ies
+docker-compose --project-name "$PROJECT_NAME" up -d --no-recreate ies
 prepare_dbms
 wait_for_service ies 1247
 start_resources aegisasu1 aegisua1 hades lucy snoopy
 
 docker exec --interactive --user irods $(container_for ies) bash <<EOS
-  echo "contents" > /home/irods/test-file
+  printf 'contents' > /home/irods/test-file
 
   iadmin atg rodsadmin ipc_admin
 
   for resc in aegisASU1Res lucyRes snoopyRes
   do
-    echo waiting for resource \$resc
-    until \$(ilsresc \$resc >/dev/null)
+    printf 'waiting for resource %s\n' "\$resc"
+    until \$(ilsresc "\$resc" >/dev/null)
     do
       sleep 1
     done
@@ -115,28 +117,28 @@ docker exec --interactive --user irods $(container_for ies) bash <<EOS
 
   for i in \$(cat <(seq 10) <(echo ☠))
   do
-    user=user-\$i
-    echo creating \$user and giving data
-    iadmin mkuser \$user rodsuser
-    iadmin moduser \$user password password
+    user=user-"\$i"
+    printf 'creating %s and giving data\n' "\$user"
+    iadmin mkuser "\$user" rodsuser
+    iadmin moduser "\$user" password password
 
-    clientUserName=\$user imkdir /iplant/home/\${user}/coll
-    clientUserName=\$user imkdir /iplant/home/\${user}/coll-☠
-    clientUserName=\$user imkdir /iplant/home/\${user}/nested
-    clientUserName=\$user imkdir /iplant/home/\${user}/nested/coll-1
-    clientUserName=\$user imkdir /iplant/home/\${user}/nested/coll-2
+    clientUserName="\$user" imkdir /iplant/home/"\$user"/coll
+    clientUserName="\$user" imkdir /iplant/home/"\$user"/coll-☠
+    clientUserName="\$user" imkdir /iplant/home/"\$user"/nested
+    clientUserName="\$user" imkdir /iplant/home/"\$user"/nested/coll-1
+    clientUserName="\$user" imkdir /iplant/home/"\$user"/nested/coll-2
 
-    clientUserName=\$user iput /home/irods/test-file /iplant/home/\${user}/file-☠
+    clientUserName="\$user" iput /home/irods/test-file /iplant/home/"\$user"/file-☠
 
     for f in \$(seq 100)
     do
-      clientUserName=\$user iput /home/irods/test-file /iplant/home/\${user}/nested/coll-2/file-\$f
+      clientUserName="\$user" \\
+      iput /home/irods/test-file /iplant/home/"\$user"/nested/coll-2/file-"\$f"
     done
  
-    clientUserName=\$user iput -R hadesRes /home/irods/test-file /iplant/home/\${user}/hades-file
+    clientUserName="\$user" iput -R hadesRes /home/irods/test-file /iplant/home/"\$user"/hades-file
   done
 
   rm --force /home/irods/test-file
 EOS
-
 
