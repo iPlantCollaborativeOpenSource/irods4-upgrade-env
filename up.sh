149e6586b405aa8f4552a429b5b5250a3982bfe7
Tony Edgin <tedgin@iplantcollaborative.org>
2016-07-23 00:54:57 -0700

    switched docker calls to docker-compose

--- up.sh
+++ up.sh
@@ -6,17 +6,21 @@
 source env.properties
 
 
-container_for()
+dc()
 {
-  local service="$1"
+  docker-compose --project-name "$PROJECT_NAME" "$@"
+}
 
-  printf '%s_%s_1' "$PROJECT_NAME" "$service"
+
+dc-up()
+{
+  dc up -d --no-recreate "$@"
 }
 
 
 dbms_psql() 
 {      
-  docker exec --user postgres $(container_for dbms) psql "$@"
+  dc exec --user postgres dbms psql "$@"
 }
 
 
@@ -33,14 +37,12 @@ wait_for_service()
   local service="$1"
   local port="$2"
 
-  local container=$(container_for "$service")
+  printf 'waiting for service %s\n' "$service"
 
-  printf 'waiting for service on %s\n' "$service"
-
-  until docker exec --interactive "$container" bash -c "exec <>/dev/tcp/localhost/'$port'" 
+  until dc exec "$service" bash -c "exec <>/dev/tcp/localhost/'$port'"
   do
     sleep 1
-  done 2>/dev/null
+  done >/dev/null 2>&1
 }
 
 
@@ -71,32 +73,29 @@ prepare_dbms()
 }
 
 
-docker-compose --project-name "$PROJECT_NAME" up -d --no-recreate dbms
-docker-compose --project-name "$PROJECT_NAME" up -d --no-recreate ies
-
-docker-compose --project-name "$PROJECT_NAME" \
-        up -d --no-recreate aegisasu1 aegisua1 hades lucy snoopy
-
+dc-up dbms
+dc-up ies
+dc-up aegisasu1 aegisua1 hades lucy snoopy
 prepare_dbms
 
-docker exec --interactive $(container_for ies) bash <<EOS
+dc exec ies bash -c "
   printf 'waiting for iRODS on ies\n'
 
   until [ -e /IRODS_READY ]
   do
     sleep 1
   done
-EOS
-  
-docker exec --interactive --user irods $(container_for ies) bash <<EOS
+"
+
+dc exec --user irods ies bash -c "
   printf 'contents' > /home/irods/test-file
 
   iadmin atg rodsadmin ipc_admin
 
   for resc in aegisASU1Res hadesRes lucyRes snoopyRes
   do
-    printf 'waiting for resource %s\n' "\$resc"
-    until \$(ilsresc "\$resc" >/dev/null)
+    printf 'waiting for resource %s\n' \"\$resc\"
+    until \$(ilsresc \"\$resc\" >/dev/null)
     do
       sleep 1
     done
@@ -112,28 +111,28 @@ docker exec --interactive --user irods $(container_for ies) bash <<EOS
 
   for i in \$(cat <(seq 2) <(echo ☠))
   do
-    user=user-"\$i"
-    printf 'creating %s and giving data\n' "\$user"
-    iadmin mkuser "\$user" rodsuser
-    iadmin moduser "\$user" password password
+    user=user-\"\$i\"
+    printf 'creating %s and giving data\n' \"\$user\"
+    iadmin mkuser \"\$user\" rodsuser
+    iadmin moduser \"\$user\" password password
 
-    clientUserName="\$user" imkdir /iplant/home/"\$user"/coll
-    clientUserName="\$user" imkdir /iplant/home/"\$user"/coll-☠
-    clientUserName="\$user" imkdir /iplant/home/"\$user"/nested
-    clientUserName="\$user" imkdir /iplant/home/"\$user"/nested/coll-1
-    clientUserName="\$user" imkdir /iplant/home/"\$user"/nested/coll-2
+    clientUserName=\"\$user\" imkdir /iplant/home/\"\$user\"/coll
+    clientUserName=\"\$user\" imkdir /iplant/home/\"\$user\"/coll-☠
+    clientUserName=\"\$user\" imkdir /iplant/home/\"\$user\"/nested
+    clientUserName=\"\$user\" imkdir /iplant/home/\"\$user\"/nested/coll-1
+    clientUserName=\"\$user\" imkdir /iplant/home/\"\$user\"/nested/coll-2
 
-    clientUserName="\$user" iput /home/irods/test-file /iplant/home/"\$user"/file-☠
+    clientUserName=\"\$user\" iput /home/irods/test-file /iplant/home/\"\$user\"/file-☠
 
     for f in \$(seq 3)
     do
-      clientUserName="\$user" \\
-      iput /home/irods/test-file /iplant/home/"\$user"/nested/coll-2/file-"\$f"
+      clientUserName=\"\$user\" \\
+      iput /home/irods/test-file /iplant/home/\"\$user\"/nested/coll-2/file-\"\$f\"
     done
  
-    clientUserName="\$user" iput -R hadesRes /home/irods/test-file /iplant/home/"\$user"/hades-file
+    clientUserName=\"\$user\" iput -R hadesRes /home/irods/test-file /iplant/home/\"\$user\"/hades-file
   done
 
   rm --force /home/irods/test-file
-EOS
+"
 
