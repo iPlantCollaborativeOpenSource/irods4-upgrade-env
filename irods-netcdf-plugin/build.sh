a465118f7bf927341622c088516750c741f29981
Tony Edgin <tedgin@iplantcollaborative.org>
2016-08-19 14:59:30 -0700

    build happens as current user

--- irods-netcdf-plugin/build.sh
+++ irods-netcdf-plugin/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker build --tag irods-dev-build .
+docker build --tag irods-netcdf-build .
 
 if [ -e src ]
 then
@@ -9,8 +9,8 @@ else
   git clone https://github.com/irods/irods_netcdf.git src
 fi
 
-docker run --rm --tty --volume=$(pwd)/src:/src --name=netcdf-builder \
-       irods-dev-build  bash -c '
+docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=netcdf-builder \
+       irods-netcdf-build  bash -c '
   for pkg in api microservices icommands
   do
     "$pkg"/packaging/build.sh clean
