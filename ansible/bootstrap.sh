59af6fb3de79ef4d41b15b02100cecb9bc74414a
Tony Edgin <tedgin@iplantcollaborative.org>
2016-08-31 12:26:58 -0700

    switched to using subtree for irods4-upgrade

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -2,12 +2,6 @@
 
 readonly Cmd="$*"
 
-if [ -f requirements.yml ]
-then
-  printf 'Installing required roles ...\n'
-  ansible-galaxy install --role-file requirements.yml
-fi
-
 if [ -z "$Cmd" ]
 then
   bash
--- build.sh
+++ build.sh
@@ -5,6 +5,20 @@ then
   ssh-keygen -f id_rsa -N ''
 fi
 
+readonly IRODS4UpgradeDir="$(dirname '$0')"/ansible/ds-ansible/irods4-upgrade
+
+readonly SetAVUPluginDir="$IRODS4UpgradeDir"/irods-setavu-plugin
+if [ -e "$SetAVUPluginDir" ]
+then
+  (cd "$SetAVUPluginDir" && git pull)
+else  
+  git clone https://github.com/iPlantCollaborativeOpenSource/irods-setavu-plugin.git \
+      "$(dirname '$0')"/ansible/ds-ansible/irods4-upgrade/irods-setavu-plugin
+fi
+(cd "$SetAVUPluginDir" && ./build.sh)
+
+(cd "$IRODS4UpgradeDir"/irods-netcdf-plugin && ./build.sh)
+
 docker-compose --project-name irods build base
 docker-compose --project-name irods build server
 docker-compose --project-name irods build
